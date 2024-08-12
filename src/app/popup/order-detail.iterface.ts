3506d0343b7d250c1b1562f4877daaaa801deb0a
harshal7321 <harshal.wandhare@neeyamoworks.com>
2024-08-12 12:51:08 +0530

    Delete src directory

--- src/app/popup/order-detail.iterface.ts
+++
@@ -1,7 +0,0 @@
-export interface Details {
-  orderId: number;
-  cartId: number;
-  itemName: string;
-  itemPrice: number;
-  itemQuantity: number;
-}
--- src/app/restuarants/restaurants.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Restaurant {
-  "restaurantId": number,
-  "restaurantName": string,
-  "restaurantLocation": string,
-  "openAt": number,
-  "closeAt": number
-}
